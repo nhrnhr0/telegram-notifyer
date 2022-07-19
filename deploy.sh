effda091ea96c7d2754e6bd04c5eca732ac8df96
nhrnhr0 <nhrnhr0@gmail.com>
2022-07-19 18:38:18 +0300

    adding delpoy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo git pull
+source ../env/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+sudo supervisorctl restart telegramcelery
+sudo supervisorctl restart telegramcelerybeat
+sudo supervisorctl restart gunicornProj
+sudo supervisorctl restart gunicornProjTelegram
+sudo service nginx restart
+sudo supervisorctl status
+echo '=========== done ==========='
